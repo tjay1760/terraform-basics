55341e631213f5c22add112a2c1dba90881aa53a
tjay1760 <mcjthiongo@gmail.com>
2026-03-22 14:24:23 +0300

    update

--- main.tf
+++ main.tf
@@ -1,7 +1,50 @@
 provider "aws" {
-region = "us-east-1"
+  region = "us-east-1"
 }
 resource "aws_instance" "example" {
-ami = "ami-0323c3dd2da7fb37d"
-instance_type = "t3.micro"
+  ami                    = "ami-02dfbd4ff395f2a1b"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+#!/bin/bash
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+
+
+cd /home/ec2-user
+echo "Hi there mate" > /var/www/html/index.html
+
+EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-security-instance"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+      from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
